4f5daa899822077a490906759562fbf39f9b5d85
Mike <wattonlaw@gmail.com>
2022-04-14 01:32:23 -0500

    commit for part 2

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 
 from crypt import methods
 from curses.ascii import US
-from flask import Flask, request, redirect, render_template
+import re
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -20,7 +21,20 @@ db.create_all()
 
 @app.route('/')
 def root():
-    return redirect("/users")
+    """Show recent posts, most recent first"""
+
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    # what is the all()?
+    return render_template("posts/homepage.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """show 404 error page if user wants non-existent page"""
+
+    return render_template('404.html'), 404
+    #  why the integer 404 after render template?
+
+############ User template
 
 @app.route('/users', methods=["GET"])
 def show_users():
@@ -46,8 +60,10 @@ def new_user_form_submit():
 
     db.session.add(new_user)
     db.session.commit()
+    flash(f"User {new_user.full_name} added.")
 
     return render_template("/users")
+    # noticed in earlier version they had this as render template, and in part 2 solution it's redirect("/users"). does it matter?
 
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
@@ -86,3 +102,65 @@ def delete_user(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+####### Posts route
+
+@app.route('/users/<int:user_id>/posts/new')
+def posts_new_form(user_id):
+    """Shows a form that creates a new post for a certain user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])    
+def submit_new_post(user_id):
+    """form submission handler for creating new post"""
+
+    user = User.query.get_or_404(user_id)
+    new_post = Post(title=request.form['title'], content=request.form['content'], user=user)
+
+    db.session.add(new_post)
+    db.session.commit()
+    flash(f"Post {'new_post.title'} added.")
+
+    return redirect(f"/users/{user_id}")
+
+@app.route('/posts/<int:post_id')
+def show_post(post_id):
+    """show info for a requested post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('/posts/show.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    """shows a form to edit existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('/posts/edit.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def update_post(post_id):
+    """handles form submission for editing an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited.")
+
+    return redirect(f"f/users/{post.user_id}")
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    """handles form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' deleted.")
+
+    return redirect(f"/users/{post.user_id}")
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from email.policy import default
+from turtle import title
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -20,7 +22,27 @@ class User(db.Model):
 
         return f"{self.first_name} {self.last_name}"
 
+class Post(db.Model):
+
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(100), nullable = False)
+    content = db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def easily_read_date(self):
+        """outputs an easy to read date"""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+        # %a is abbreviated day, %b is abbreviated month, %-d is day of month as decimal (1, 2, 3...), %Y is full 4 number year, %-I is hour number, %M is minute number with a 0 in front of it for the first number, %p is AM or PM
+
+
 def connect_db(app):
+    """connects this database to provided Flask app, needs to be called in app"""
+
     db.app = app
     db.init_app(app)
     # don't understand this
